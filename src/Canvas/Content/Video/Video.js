d850276139be2b66527caa590a2c0fc2a29bcbe0
mporubovic <mawej1@gmail.com>
2022-07-17 00:07:16 +0100

    play video on hover, pause on mouse leave

--- src/Canvas/Content/Video/Video.js
+++ src/Canvas/Content/Video/Video.js
@@ -2,8 +2,13 @@ import './Video.sass'
 import {forwardRef} from "react";
 
 export default forwardRef((props, ref) => {
+
+
+
     return (
-        <video autoPlay controls loop muted
+        <video controls loop
+               onMouseEnter={(e) => e.target.play()}
+               onMouseOut={(e) => e.target.pause()}
                ref={ref}
                className="video"
                src={window.apiURL + props.src}
